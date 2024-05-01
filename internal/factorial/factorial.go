cebbfad76c0a93ed1b3cfaaa3ea404611bbf6ad7
Yehor Chervony <chervony.egor@gmail.com>
2024-05-01 11:21:45 +0300

    Changed, using repository pattern

--- internal/factorial/factorial.go
+++ internal/factorial/factorial.go
@@ -1,8 +1,17 @@
 package factorial
 
-func Calculate(n int) int {
+type IFactorialRepository interface {
+	Calculate(n int) int
+}
+
+type FactorialRepository struct{}
+
+func NewFactorialRepository() *FactorialRepository {
+	return &FactorialRepository{}
+}
+func (f *FactorialRepository) Calculate(n int) int {
 	if n == 0 {
 		return 1
 	}
-	return n * Calculate(n-1)
+	return n * f.Calculate(n-1)
 }
