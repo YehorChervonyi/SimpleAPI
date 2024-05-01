498babbfd26e1192187f15da55c429dc1a1eeaf7
Yehor Chervony <chervony.egor@gmail.com>
2024-05-01 11:38:40 +0300

    Using config loading, start server on port in config

--- cmd/calculation-server/main.go
+++ cmd/calculation-server/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/YehorChervonyi/SimpleAPI/config"
 	"github.com/YehorChervonyi/SimpleAPI/internal/routes"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+	cfg := config.LoadConfig()
+	port := strconv.Itoa(cfg.Server.Port)
 	router := httprouter.New()
 	routes.InitRoutes(router)
-	fmt.Println("Satarting server on post 8989...")
-	http.ListenAndServe(":8989", router)
+	fmt.Printf("Satarting server on post %s...", port)
+	http.ListenAndServe(":"+port, router)
 }
